47629ee8fa5281b5e8ad4990a720dedf4c19b502
Silas-Coleman <Silasc2401@gmail.com>
2024-03-23 13:29:15 -0500

    Added Spotify Auth File

---
+++ spotify_auth.py
@@ -0,0 +1,183 @@
+'''
+    Manages spotify auth.
+    Uses Authroization code flow as described at
+
+    https://developer.spotify.com/web-api/authorization-guide/
+
+    Usage:
+
+        spauth = SpotifyAuth()
+
+        token = spauth.get_fresh_token(auth_code)
+
+    Thanks to https://github.com/plamere/SmarterPlaylists/blob/master/server/spotify_auth.py
+'''
+
+import sys
+import os
+import time
+import simplejson as json
+import requests
+import redis
+
+class SpotifyAuth(object):
+
+    def __init__(self, r = None):
+        if r:
+            self.r = r
+        else:
+            self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        self.EXPIRES_THRESHOLD = 120
+        self.client_id = os.environ.get('SPOTIPY_CLIENT_ID')
+        self.client_secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
+        self.client_redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI')
+        self.trace = False
+
+        print('client id', self.client_id)
+        print('client secret', self.client_secret)
+        print('client redirect uri', self.client_redirect_uri)
+
+        if self.client_id == None or self.client_secret == None or \
+            self.client_redirect_uri == None:
+            raise Exception('Missing SPOTIPY credentials in the environment')
+
+
+    def get_fresh_token(self, code):
+        now = time.time()
+        token = self._get_token(code)
+        if token:
+            if (token['expires_at'] - now) < self.EXPIRES_THRESHOLD:
+                token = self._refresh_token(token)
+                if token:
+                    self._add_token(code, token)
+        else:
+            token = self._add_auth_code(code)
+
+        return token
+
+    def get_fresh_token_for_user(self, user):
+        now = time.time()
+        token = self._get_token(user)
+        if token:
+            if (token['expires_at'] - now) < self.EXPIRES_THRESHOLD:
+                token = self._refresh_token(token)
+                if token:
+                    self._add_token(code, token)
+        return token
+
+    def _add_auth_code(self, auth_code):
+        token = self._get_new_token(auth_code)
+        if token:
+            token = self._add_token(auth_code, token)
+        return token
+
+    def _add_token(self, code, token):
+        now = time.time()
+        token['expires_at'] = int(now) + token['expires_in']
+        user_info = self._me(token)
+        if user_info and 'id' in user_info:
+            token['user_id'] = user_info['id']
+            token['user_name'] = user_info['display_name']
+
+            user = token['user_id']
+            self._put('token:' + code, token)
+            if self.trace:
+                print('added token to db', code, token)
+        else:
+            print("can't get user info, bailing")
+            return None
+        return token
+
+
+    def _get_new_token(self, authorization_code):
+        params = {
+            'grant_type': 'authorization_code',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'redirect_uri': self.client_redirect_uri,
+            'code' : authorization_code
+        }
+        r = requests.post('https://accounts.spotify.com/api/token', params)
+        if r.status_code >= 200 and r.status_code < 300:
+            token = r.json()
+            if self.trace:
+                print('got back token', token)
+            return token
+        else:
+            return None
+
+    def _get_token(self, code):
+        key = 'token:' + code
+        js = self._get(key)
+        return js
+
+    def _refresh_token(self, token):
+        params = {
+            'grant_type': 'refresh_token',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'refresh_token' : token['refresh_token']
+        }
+        r = requests.post('https://accounts.spotify.com/api/token', params)
+        if r.status_code >= 200 and r.status_code < 300:
+            new_token = r.json()
+            if self.trace:
+                print('got back token', token)
+            if not 'refresh_token' in new_token:
+                new_token['refresh_token'] = token['refresh_token']
+            return new_token
+        else:
+            raise Exception("can't get token")
+
+
+    def _me(self, token):
+        me_info = self._spget('me', token['access_token'])
+        return me_info
+
+    def _spget(self, method, auth, params=None):
+        prefix = 'https://api.spotify.com/v1/'
+        args = dict(params=params)
+        url = prefix + method
+        headers = {'Authorization': 'Bearer {0}'.format(auth)}
+        headers['Content-Type'] = 'application/json'
+
+        r = requests.get(url, headers=headers, **args)
+
+        if len(r.text) > 0:
+            results = r.json()
+            if self.trace:  # pragma: no cover
+                print('RESP', results)
+                print()
+            return results
+        else:
+            return None
+
+    def _get(self, key):
+        js = self.r.get(key)
+        if js:
+            return json.loads(js)
+        else:
+            return None
+
+    def _put(self, key, val):
+        js = json.dumps(val)
+        self.r.set(key, js)
+
+    def delete_auth(self, code):
+        key = 'token:' + code
+        self.r.delete(key)
+    
+
+
+if __name__ == '__main__':
+    spauth = SpotifyAuth()
+    arg = sys.argv[1]
+    fields = arg.split('=')
+    my_code = fields[-1]
+    spauth.delete_auth(my_code)
+    token = spauth.get_fresh_token(my_code)
+    if token:
+        remaining = token['expires_at'] - time.time()
+        print('Got a token that expires in', remaining, 'seconds')
+    else:
+        print('no token for that code')
