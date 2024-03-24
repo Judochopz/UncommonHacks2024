8f20cbef944e0b9991fd29eea4492b7d0d8ca873
Judochopz <114613397+Judochopz@users.noreply.github.com>
2024-03-24 01:38:30 -0500

    Potential multi-threading available

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,6 @@
 import spotify_auth
 import pandas as pd
+from multiprocessing.pool import ThreadPool 
 
 def main(userID):
     # Take in spotipy object and user object
@@ -28,6 +29,7 @@ def main(userID):
     for track in top_songs['items']:
         top_ten_songs.add(track['id'])
         related_artists = sp.artist_related_artists(track['artists'][0]['id'])['artists']
+
         for artist in related_artists[:5]:
             top_related_artists.add(artist['id'])
         track_audio_features = sp.audio_features(track['id'])[0]
@@ -38,27 +40,34 @@ def main(userID):
             track_vectors[feature].append(track_audio_features[feature])
         track_vectors['genre'].append(set(sp.artists([track['artists'][0]['id']])['artists'][0]['genres']))
     track_history = pd.DataFrame(track_vectors)
-    potential_track_vectors = dict()
-    potential_track_vectors['name'] = []
-    potential_track_vectors['artist'] = []
-    potential_track_vectors['artist_id'] = []
-    for feature in track_features:
-        potential_track_vectors[feature] = []
-    potential_track_vectors['genre'] = []
-    while top_related_artists:
-        artist_tracks = sp.artist_top_tracks(top_related_artists.pop())['tracks']
-        for track in artist_tracks:
-            if track['id'] in top_ten_songs:
-                continue
-            track_audio_features = sp.audio_features(track['id'])[0]
-            potential_track_vectors['name'].append(track['name'])
-            potential_track_vectors['artist'].append(track['artists'][0]['name'])
-            potential_track_vectors['artist_id'].append(track['artists'][0]['id'])
-            for feature in track_features:
-                potential_track_vectors[feature].append(track_audio_features[feature])
-            potential_track_vectors['genre'].append(set(sp.artists([track['artists'][0]['id']])['artists'][0]['genres']))
-    potential_recommended = pd.DataFrame(potential_track_vectors)
-
+    related_artist_list = list(top_related_artists)
+    # Function for potential recommended songs to allow multi-threading
+    def potential_recommended_songs(artist_list, top_ten_songs = top_ten_songs):
+        potential_track_vectors = dict()
+        potential_track_vectors['name'] = []
+        potential_track_vectors['artist'] = []
+        potential_track_vectors['artist_id'] = []
+        for feature in track_features:
+            potential_track_vectors[feature] = []
+        potential_track_vectors['genre'] = []
+        for artist in artist_list:
+            artist_tracks = sp.artist_top_tracks(artist)['tracks']
+            for track in artist_tracks:
+                if track['id'] in top_ten_songs:
+                    continue
+                track_audio_features = sp.audio_features(track['id'])[0]
+                potential_track_vectors['name'].append(track['name'])
+                potential_track_vectors['artist'].append(track['artists'][0]['name'])
+                potential_track_vectors['artist_id'].append(track['artists'][0]['id'])
+                for feature in track_features:
+                    potential_track_vectors[feature].append(track_audio_features[feature])
+                potential_track_vectors['genre'].append(set(sp.artists([track['artists'][0]['id']])['artists'][0]['genres']))
+        return pd.DataFrame(potential_track_vectors)
+    # Multi-threading for faster API calling
+    # with ThreadPool(1) as p:
+    #     vec = p.map(potential_recommended_songs, related_artist_list)
+    # potential_recommended = pd.concat(vec)
+    potential_recommended = potential_recommended_songs(related_artist_list)
     track_history.to_csv("sampleCSVs/track_history.csv")
     potential_recommended.to_csv("sampleCSVs/potential_recommended.csv")
 
